a2d87ed1a042a271475122703edf7ae7c5154afc
user.name <oscar.sala@epn.edu.ec>
2025-06-07 17:54:41 -0500

    Se actuelizó la tarjeta habitacion con iconos

--- hostel/src/components/HabitacionCard.jsx
+++ hostel/src/components/HabitacionCard.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './HabitacionCard.css';
+import { FaBed, FaShower, FaCar, FaPaw } from 'react-icons/fa';
 
-const HabitacionCard = ({ titulo, precio, descripcion, imagen }) => {
+const HabitacionCard = ({ titulo, descripcion, imagen, precio, camas, banos, parqueo, mascotas }) => {
   return (
     <div className="habitacion-card">
       <img src={imagen} alt={titulo} className="habitacion-img" />
+
       <div className="habitacion-info">
-        <p className="habitacion-precio">{precio}</p>
         <h3 className="habitacion-titulo">{titulo}</h3>
         <p className="habitacion-desc">{descripcion}</p>
+
+        <div className="habitacion-iconos">
+          <div><FaBed /> <span>{camas}</span></div>
+          <div><FaShower /> <span>{banos}</span></div>
+          <div><FaCar /> <span>{parqueo}</span></div>
+          <div><FaPaw /> <span>{mascotas}</span></div>
+        </div>
+
+        <div className="habitacion-precio">{precio}</div>
       </div>
     </div>
   );
 };
 
 export default HabitacionCard;
-
