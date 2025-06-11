83a3165b52fec3c4b8d710b6bf785215861447b5
user.name <oscar.sala@epn.edu.ec>
2025-06-11 15:13:26 -0500

    s

--- hostel/src/pages/HabitacionDetalle.jsx
+++ hostel/src/pages/HabitacionDetalle.jsx
@@ -7,8 +7,10 @@ import HabitacionCardGeneral from '../components/HabitacionCardGeneral';
 import HabitacionInfo from '../components/HabitacionInfo';
 import HabitacionDescripcion from '../components/HabitacionDescripcion';
 import './HabitacionDetalle.css';
+import { useNavigate } from 'react-router-dom';
 
 const HabitacionDetalle = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const habitacion = habitaciones.find(h => h.id === id);
 
@@ -28,6 +30,7 @@ const HabitacionDetalle = () => {
     <div>
       <Header />
       <div className="detalle-container">
+        <button className="btn-atras" onClick={() => navigate(-1)}>Atrás</button>
         <HabitacionCardGeneral imagenes={imagenes} />
         <div className="detalle-info-wrapper">
           <div className="detalle-izquierda">
